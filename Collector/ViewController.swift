7d0e38dc70fad92bc501815889f317420e5bee9f
Rob McDonnell <rob.f.mcdonnell@outlook.com>
2017-10-04 11:36:07 -0400

    Initial Commit

---
+++ Collector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Collector
+//
+//  Created by Rob McDonnell on 10/4/17.
+//  Copyright © 2017 Rob McDonnell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
